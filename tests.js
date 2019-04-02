497a4339eceedf86c36597e9fe63110268fa5e68
Антон Лукьянов <antonluk0852@gmail.com>
2019-04-02 16:00:51 +0500

    777

---
+++ tests.js
@@ -0,0 +1,34 @@
+describe("Эльф должен делать простые движения", function() {
+	it("поднять левую руку", function(done) {
+		let elf = {
+			danceSpeed: 10,
+			stance: [0, 0, 0, 0],
+		}
+
+		leftHandUp(elf).then((elf) => {
+			expect(elf.stance).toEqual([1, 0, 0, 0]);
+			done();
+		})
+	});
+
+	it("поднять правую руку", function(done) {
+		let elf = {
+			danceSpeed: 10,
+			stance: [0, 0, 0, 0],
+		}
+		rightHandUp(elf).then((elf) => {
+			expect(elf.stance).toEqual([0, 1, 0, 0]);
+			done();
+		})
+	});
+	it("Работать обеими руками", function(done) {
+		let elf = {
+			danceSpeed: 10,
+			stance: [1, 0, 0, 0],
+		}
+		rightHandUp(elf).then((elf) => {
+			expect(elf.stance).toEqual([1, 1, 0, 0]);
+			done();
+		})
+	});
+});
